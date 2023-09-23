684d26b2ad96540be37c01d8003d6d853b3a2151
cuong <duongcuong96@gmail.com>
2023-09-23 15:28:24 +0700

    update plugin desc

--- wp-bos-feedback.php
+++ wp-bos-feedback.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: BOS feedback on NEAR Protocol
- * Author: cuongdcdev.near
- * Description: Save user's feedback from BOS
+ * Author: <a href="https://cuongdcdev.near.social/">cuongdcdev.near</a> | <a href="https://near.org/near/widget/ComponentDetailsPage?src=cuongdcdev.near/widget/BOSFeedback">View detail</a>
+ * Description: Save user's feedback from BOS - Blockchain Operating System
  * Version: 1.0
  */
 require_once("inc/setup.php");
