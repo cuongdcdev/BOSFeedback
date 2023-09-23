5d1f290a102009078e9096ef85d239b3c2d4a0b0
cuong <duongcuong96@gmail.com>
2023-09-23 15:03:18 +0700

    bug fix

--- inc/router.php
+++ inc/router.php
@@ -7,10 +7,13 @@ add_action('rest_api_init', function ($params) {
     ));
 });
 
-function bos_new_feedback($params){
+function bos_new_feedback($params)
+{
 
     $cmt = $params->get_params();
 
+    // var_dump( $cmt );
+    // die;
     if (empty($cmt["message"]) || empty($cmt["contact_addr"])) {
         wp_send_json([
             "status" => "error",
@@ -19,7 +22,7 @@ function bos_new_feedback($params){
         exit;
     }
 
-    $term_name =  isset($cmt["project_name"]) ? strtolower(strip_tags(trim($_POST["project_name"]))) : "uncategorized";
+    $term_name =  isset($cmt["project_name"]) ? strtolower(strip_tags(trim($cmt["project_name"]))) : "uncategorized";
     $parent_term = term_exists($term_name, 'bos_fb_tax');
     $message = isset($cmt["message"]) ? (strip_tags(trim($cmt["message"]))) : "";
     $contact_addr = isset($cmt["contact_addr"]) ? (strip_tags(trim($cmt["contact_addr"]))) : false;
@@ -27,29 +30,27 @@ function bos_new_feedback($params){
 
     //create new term if not exist 
     if (!is_array($parent_term)) {
-        $parent_term_id = $parent_term['term_id']; // get numeric term id
+        $parent_term_id = isset($parent_term) ? $parent_term['term_id'] : ''; // get numeric term id
 
         wp_insert_term(
             $term_name,   // the term 
             'bos_fb_tax', // the taxonomy
             array(
-                'parent'      => $parent_term_id,
+                'parent' => $parent_term_id,
             )
         );
     }
-
 
     //insert feedback + term 
     $pid = wp_insert_post([
         'post_title'    => wp_trim_words($message, 100, "...") . " - " . $contact_addr,
         'post_content'  => $message,
         'post_status'   => 'publish',
-        'post_author'   => 1, // Replace with the author ID
-        'post_type'     => 'bos_fb', // Replace with your custom post type slug
+        'post_type'     => 'bos_fb', 
     ]);
 
     if (!is_wp_error($pid)) {
-        wp_set_post_terms($pid, $term_name, 'bos_fb_tax'); // Replace with your custom category and taxonomy
+        wp_set_post_terms($pid, $term_name, 'bos_fb_tax'); 
         update_post_meta($pid, "bos_contact_addr", $contact_addr);
         update_post_meta($pid, "bos_wallet_addr", $wallet_addr);
 
