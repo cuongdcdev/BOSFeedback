f277d85a24cace6b218d28680f172f019590f816
cuong <duongcuong96@gmail.com>
2023-09-23 15:24:06 +0700

    update plugin name

--- wp-bos-feedback.php
+++ wp-bos-feedback.php
@@ -1,10 +1,9 @@
-<?php 
+<?php
 /**
- * Plugin Name: BOS feedback
+ * Plugin Name: BOS feedback on NEAR Protocol
  * Author: cuongdcdev.near
  * Description: Save user's feedback from BOS
+ * Version: 1.0
  */
- 
-
- require_once("inc/setup.php");
+require_once("inc/setup.php");
 require_once("inc/router.php");
